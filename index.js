8e4f3c0c408feaef07f433bb7ba6d2e487752a73
Ghanashyam Thakur <109438604+SamkrGT@users.noreply.github.com>
2023-01-23 16:38:23 +0530

    Final Upload
    Final Upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener("keypress", function(event) {
+
+  var randomNumber1 = Math.trunc(Math.random()*6)+1;
+  var randomNumber2 = Math.trunc(Math.random()*6)+1;
+
+  document.querySelector("img.img1").setAttribute("src" , "images/dice" + randomNumber1 +".png");
+  document.querySelector("img.img2").setAttribute("src" , "images/dice" + randomNumber2 +".png");
+
+  if(randomNumber1>randomNumber2)
+  document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+  else if(randomNumber2>randomNumber1)
+  document.querySelector("h1").innerHTML="Player 2 wins!🚩";
+  else
+  document.querySelector("h1").innerHTML="Draw";
+
+});
